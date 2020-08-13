d1ddb1ae5dde1b8a036d01ea83c1c0fc5d323b53
tango <tango@DESKTOP-RJ4TJB7>
2020-08-14 00:04:35 +0800

    增加mysql数据库建库建表脚本

---
+++ studentinfo-manager-sys/src/main/resources/database.sql
@@ -0,0 +1,13 @@
+create database student_info;
+
+use student_info;
+
+CREATE TABLE IF NOT EXISTS `student`(
+   `studentId` INT UNSIGNED AUTO_INCREMENT,
+   `studentNumber` VARCHAR(20) NOT NULL,
+   `name` VARCHAR(20) NOT NULL,
+   `age` int NOT NULL,
+   `phone` VARCHAR(20) NOT NULL,
+   `address` VARCHAR(20) NOT NULL,
+   PRIMARY KEY ( `studentId` )
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
